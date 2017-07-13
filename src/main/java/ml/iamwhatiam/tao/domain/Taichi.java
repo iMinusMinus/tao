667448673774719258a5f442c4c540cd3f3fc847
iMinusMinus <mean.leung@outlook.com>
2017-07-13 11:35:04 +0800

    add getter and setter

--- src/main/java/ml/iamwhatiam/tao/domain/Taichi.java
+++ src/main/java/ml/iamwhatiam/tao/domain/Taichi.java
@@ -69,4 +69,45 @@ public abstract class Taichi implements Serializable {
 	 * last revised time
 	 */
 	protected Date lastModifiedDate;
+
+	public long getOid() {
+		return oid;
+	}
+
+	public void setOid(long oid) {
+		this.oid = oid;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getLastModifiedBy() {
+		return lastModifiedBy;
+	}
+
+	public void setLastModifiedBy(String lastModifiedBy) {
+		this.lastModifiedBy = lastModifiedBy;
+	}
+
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(Date lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+	
 }
