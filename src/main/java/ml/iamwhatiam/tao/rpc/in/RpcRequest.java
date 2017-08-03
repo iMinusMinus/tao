5425c6b1658f357f61644cf47dfea9967fe44f5d
iMinusMinus <mean.leung@outlook.com>
2017-08-03 10:48:29 +0800

    rpc abstract request and response

--- src/main/java/ml/iamwhatiam/tao/rpc/in/RpcRequest.java
+++ src/main/java/ml/iamwhatiam/tao/rpc/in/RpcRequest.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package ml.iamwhatiam.tao.rpc;
+package ml.iamwhatiam.tao.rpc.in;
 
 import java.io.Serializable;
 
